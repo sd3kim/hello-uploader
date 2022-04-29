81a8fb3136491b3e3b95a6e6345e505622bc7819
sd3kim <sd3kim@gmail.com>
2022-04-29 09:36:52 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+require("dotenv").config();
+require("./backend/config/database");
+
+const app = express();
+require("./backend/config/database");
+
+const port = process.env.PORT || 3001;
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
